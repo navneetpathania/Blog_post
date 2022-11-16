31b60b9cf861f5531babaef175dc95c8b7ab0896
navneet pathania <navneetpathania210@gmail.com>
2022-11-15 20:51:02 -0500

    content editor added

--- blogpost/blog/models.py
+++ blogpost/blog/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.urls import reverse
+from ckeditor.fields import RichTextField
 class Post(models.Model):
 	title = models.CharField(max_length=100)
-	content = models.TextField()
+	content = RichTextField(blank=True,null=True)
 	date_posted = models.DateTimeField(default=timezone.now)
 	user = models.ForeignKey(User,on_delete=models.CASCADE)
 
